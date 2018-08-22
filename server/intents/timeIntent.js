297f8d022d67df1b0c01da9594ebb21e36b06473
darioestupinan <dario.estupinan@gmail.com>
2018-08-21 21:49:25 -0500

    connecting to iris-time service

--- server/intents/timeIntent.js
+++ server/intents/timeIntent.js
@@ -1,4 +1,5 @@
 'use strict';
+const request = require('superagent');
 
 module.exports.process = function process(intentData, cb) {
 
@@ -8,5 +9,17 @@ module.exports.process = function process(intentData, cb) {
     if (!intentData.location){
         return new cb(new Error('Missing location in time intent'));
     }
-    return cb(false, `I don't know the time yet in ${intentData.location[0].value}`);
+    const location = intentData.location[0].value;
+    console.log(location);
+    request
+        .get(`http://localhost:3001/service/${location}`, (err, res) => {
+            if (err || err.statusCode !== 200 || !res.body.result) {
+                console.log(err);
+                console.log(res.body);
+
+                return cb(false, `Had a problem finding out the time in ${location}`);
+            }
+
+            return cb(false, `In ${location}, it is now ${res.body.result}`);
+        });
 };
