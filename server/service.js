bec2ae0ec8fb409050e5e618d8c52fa2b2112e80
darioestupinan <dario.estupinan@gmail.com>
2018-08-23 20:30:03 -0500

    adding service registry and integrating

--- server/service.js
+++ server/service.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const service = express();
+const ServiceRegistry = require('./serviceRegistry');
+const serviceRegistry = new ServiceRegistry();
 
 service.put('/service/:intent/:port', (req, res, next) => {
     const serviceIntent = req.params.intent;
@@ -11,6 +13,7 @@ service.put('/service/:intent/:port', (req, res, next) => {
                             `[${req.connection.remoteAddress}]` :
                             req.connection.remoteAddress;
 
+    serviceRegistry.add(serviceIntent, serviceIp, servicePort);
     res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`});
 });
 
---
+++ server/serviceRegistry.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const moment = require('moment');
+const config = require('../config');
+
+class ServiceRegistry {
+
+    constructor () {
+        this._services = [];
+        this._timeout = config.registryTimeout;
+    }
+
+    add(intent, ip, port) {
+        const key = intent+ip+port;
+
+        if (!this._services[key]) {
+            this._services[key] = {
+                timestamp: Math.floor(new moment().valueOf()),
+                ip: ip,
+                port: port,
+                intent: intent
+            };
+
+        }
+        else {
+            this._services[key].timestamp = new moment().valueOf();
+        }
+        console.log(`service for ${JSON.stringify(this._services[key])}`);
+        this._cleanup();
+    }
+
+    remove(intent, ip, port){
+        const key = intent+ip+port;
+
+        delete this._services[key];
+    }
+
+    get(intent) {
+        this._cleanup();
+        for(let key in this._services) {
+            if (this._services[key].intent === intent){
+                return this._services[key];
+            }
+        }
+
+        return null;
+    }
+
+    _cleanup() {
+        const now = new moment().valueOf();
+
+        for(let key in this._services) {
+            if (this._services[key].timestamp + this._timeout < now){
+                console.log(`removed services for intent ${JSON.stringify(this._services[key])}`);
+                delete this._services[key];
+            }
+        }
+    }
+}
+
+module.exports = ServiceRegistry;
